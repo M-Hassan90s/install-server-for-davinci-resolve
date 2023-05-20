aa7c1eafd6fc2e994ab20388f5195b2f65c45dc1
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2023-05-20 17:43:58 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,8 @@
 echo "aggiorno la lista dei pacchetti"
 sudo apt update && sudo apt full-upgrade -y;
 echo "aggiungo il link per installare pgadmin"
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list';
+wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null;
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg;
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update';
 echo "installo postgresql"
